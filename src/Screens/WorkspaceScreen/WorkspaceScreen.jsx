1618ff8111b3f7f8df0403467491ad25fc559fba
matu.dev <mati.a.gimenez2002@gmail.com>
2025-11-05 11:27:55 -0300

    Clase 26

--- src/Screens/WorkspaceScreen/WorkspaceScreen.jsx
+++ src/Screens/WorkspaceScreen/WorkspaceScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ChannelSidebar from '../../Components/ChannelSidebar/ChannelSidebar'
+import ChannelDetail from '../../Components/ChannelDetail/ChannelDetail'
 
 const WorkspaceScreen = () => {
   return (
     <div>
         <ChannelSidebar/>
+        <ChannelDetail/>
     </div>
   )
 }
